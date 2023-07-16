c942358258762a58fcc4bfeb83a69e2aba8f5922
joserenter1a <josevt209@gmail.com>
2023-07-16 14:41:58 -0700

    Working RT video calling and UID

---
+++ src/components/VideoRoom.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import { VideoPlayer } from './VideoPlayer';
+
+const APP_ID = '5f41eed412454d93ab8e2d9b61430bc6';
+const TOKEN = '007eJxTYLi/atX1aKbE+t2/Eu5XuiSouGq2XJ/GO1F/scJW4TVdSjsUGEzTTAxTU1NMDI1MTE1SLI0TkyxSjVIsk8wMTYwNkpLNUl3XpzQEMjJMLBdlYWSAQBCfjaEktbCgKJ+BAQBzzh9n'
+const CHANNEL = 'teqpro';
+
+const client = AgoraRTC.createClient({
+  mode: 'rtc',
+  codec: 'vp8',
+});
+
+export const VideoRoom = () => {
+  const [users, setUsers] = useState([]);
+  const [localTracks, setLocalTracks] = useState([]);
+
+  const handleUserJoined = async (user, mediaType) => {
+    await client.subscribe(user, mediaType);
+
+    if (mediaType === 'video') {
+      setUsers((previousUsers) => [...previousUsers, user]);
+    }
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play()
+    }
+  };
+
+  const handleUserLeft = (user) => {
+    setUsers((previousUsers) =>
+      previousUsers.filter((u) => u.uid !== user.uid)
+    );
+  };
+
+  useEffect(() => {
+    client.on('user-published', handleUserJoined);
+    client.on('user-left', handleUserLeft);
+
+    client
+      .join(APP_ID, CHANNEL, TOKEN, null)
+      .then((uid) =>
+        Promise.all([
+          AgoraRTC.createMicrophoneAndCameraTracks(),
+          uid,
+        ])
+      )
+      .then(([tracks, uid]) => {
+        const [audioTrack, videoTrack] = tracks;
+        setLocalTracks(tracks);
+        setUsers((previousUsers) => [
+          ...previousUsers,
+          {
+            uid,
+            videoTrack,
+            audioTrack,
+          },
+        ]);
+        client.publish(tracks);
+      });
+
+    return () => {
+      for (let localTrack of localTracks) {
+        localTrack.stop();
+        localTrack.close();
+      }
+      client.off('user-published', handleUserJoined);
+      client.off('user-left', handleUserLeft);
+      client.unpublish(tracks).then(() => client.leave());
+    };
+  }, []);
+
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 200px)',
+        }}
+      >
+        {users.map((user) => (
+          <VideoPlayer key={user.uid} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
