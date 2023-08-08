4790661c84670cba906153c3ec6fec174e2d7093
joserenter1a <josevt209@gmail.com>
2023-08-07 17:37:37 -0700

    fix push

--- video_func/server.js
+++ video_func/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
-
-const PORT = 5173
-const ADDRESS = `localhost`
+import response  from 'express/lib/response.js';
 const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+import path from 'path'
+
+const PORT = 5172
+const ADDRESS = `localhost`;
 
 app.get('/home', (request, response) => {
     response 
@@ -11,7 +17,38 @@ app.get('/home', (request, response) => {
         .sendFile(path.join(__dirname, 'index.html'));
     });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://${ADDRESS}:${PORT}`);
+app.get('/chat', (request, response) => {
+  response
+    .status(200)
+    .setHeader('Content-type', 'text/html')
+    .sendFile(__dirname + 'socket.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log(`message: ${msg}`);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+})
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port http://${ADDRESS}:${PORT}`);
+  });
