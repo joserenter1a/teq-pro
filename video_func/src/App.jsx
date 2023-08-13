d62b0b46f01be30dfeb31f7908e9026f5df5a825
Joseph Macalinao <jhmac121101@gmail.com>
2023-08-13 15:03:01 -0700

    looking nice

--- video_func/src/App.jsx
+++ video_func/src/App.jsx
@@ -5,6 +5,8 @@ import CodeCompiler from '../../compiler/CodeCompiler';
 
 function App() {
   const [joined, setJoined] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [text, setText] = useState('Input Room Code Here');
 
   const [code, setCode] = React.useState(
     `function add(a, b) {\n  return a + b;\n}`
@@ -13,7 +15,28 @@ function App() {
     <div className="App">
       <h1>TeqPro Virtual Call</h1>
       {!joined ? (
+        <div>
+          <nav className='navbar'>
+            <button className='hamb' onClick={()=>setNavbarOpen((prev)=>!prev)}>
+              {navbarOpen ?
+              <ul>
+                Settings
+                <li className='hambel'>
+                  <button>Help</button>
+                </li>
+                <li className='hambel'>
+                  <button>HowTo</button>
+                </li>
+                </ul> 
+              :'Settings'}
+            </button>
+          </nav>
+        <input type="text" className='room-input' value = {text}
+        onClick={e=> setText('')}
+        onChange={e=> setText(e.target.value)}
+        />
         <button onClick={() => setJoined(true)}>Join Room</button>
+        </div>
       ) : (
         <div className="container">
           <div className="video-container">
